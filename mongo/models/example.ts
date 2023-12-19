0fe778132dff553651e562a4497586caad8d06e0
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-12-19 14:44:05 -0500

    set PostgreSQL and add routes

--- mongo/models/example.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectId } from "mongodb";
-
-export default class Example {
-  constructor(
-    public field1: string,
-    public field2: Array<string>,
-    public field3: number,
-    public id?: ObjectId) {}
-}
